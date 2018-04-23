ff60c5e0c7e666e9b139f9fa6ff3440af91d9674
durranee <durranee@hotmail.com>
2018-04-23 22:59:32 +0100

    header and statement body printing with tests passing

--- BankAccount.Tests/BankAccount.cs
+++ BankAccount.Tests/BankAccount.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;  
 
 namespace BankAccount.Tests
@@ -6,20 +8,53 @@ namespace BankAccount.Tests
     [TestClass]
     public class BankAccountTest
     {   
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            StreamWriter standardOut =
+                new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            Console.SetOut(standardOut);
+        }
+
+
+        [TestMethod]
+        public void BalanceShouldBeZeroAtStart()
+        {
+            BankAccount account = new BankAccount();
+            Assert.AreEqual(0, account.GetBalance());
+        }
+
         [TestMethod]
         public void AddGivenAmountToBalance()
         {
             BankAccount account = new BankAccount();
-            account.Credit(1000);
+            account.Credit(DateTime.ParseExact("20/02/2018", "dd/MM/yyyy", null), 1000);
             Assert.AreEqual(1000, account.GetBalance());
         }
+
+        //[TestMethod]
+        //public void ExpectCreditToCreateATransactionLog()
+        //{
+        //    BankAccount account = new BankAccount();
+        //    account.Credit(DateTime.ParseExact("20/02/2018", "dd/MM/yyyy", null), 1000);
+        //    Transaction obj = new Transaction()
+        //    {
+        //        date = DateTime.ParseExact("20/02/2018", "dd/MM/yyyy", null),
+        //        credit = 1000,
+        //        debit = 0,
+        //        balance = 1000
+        //    };
+        //    Assert.AreEqual<Transaction>(obj, account.statement[0]);
+        //}
+
 
         [TestMethod]
         public void TakeAwayGivenAmountFromBalance()
         {
             BankAccount account = new BankAccount();
-            account.Credit(1000);
-            account.Debit(200);
+            account.Credit(DateTime.ParseExact("21/04/2018", "dd/MM/yyyy", null), 1000);
+            account.Debit(DateTime.ParseExact("22/04/2018", "dd/MM/yyyy", null), 200);
             Assert.AreEqual(800, account.GetBalance());
         }
 
@@ -29,11 +64,26 @@ namespace BankAccount.Tests
             try
             {
                 BankAccount account = new BankAccount();
-                account.Credit(100);
-                account.Debit(2000);
+                account.Credit(DateTime.ParseExact("20/04/2018", "dd/MM/yyyy", null), 100);
+                account.Debit(DateTime.ParseExact("22/04/2018", "dd/MM/yyyy", null), 2000);
             }catch(Exception ex)
             {
                 Assert.AreEqual("Insufficient Funds.", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ItShouldPrintTransactionHistory()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                BankAccount account = new BankAccount();
+                account.Credit(DateTime.ParseExact("20/04/2018", "dd/MM/yyyy", null), 2000);
+                account.PrintStatement();
+                string expected = string.Format("{0,-12} || {1,10} || {2,-10} || {3,10}\n", "Date", "Credit", "Debit", "Balance");
+                expected += string.Format("{0,-12} || {1,10} || {2,-10} || {3,10}\n", "04/20/2018", "2000", "0", "2000");
+                Assert.AreEqual(expected, sw.ToString());
             }
         }
     }
--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace BankAccount
 {
+    public class Transaction
+    {
+        public DateTime date { get; set; }
+        public decimal credit { get; set; }
+        public decimal debit { get; set; }
+        public decimal balance { get; set; }
+
+    }
+
     public class BankAccount
     {
         decimal balance;
+        public List<Transaction> statement = new List<Transaction>();
+
 
         public BankAccount()
         {
@@ -14,16 +27,50 @@ namespace BankAccount
             return balance;
         }
 
-        public void Credit(decimal amount)
+        public void Credit(DateTime _date, decimal _amount)
         {
-            balance += amount;    
+            balance += _amount;
+            statement.Add(new Transaction() { date = _date, credit = _amount, debit = 0, balance = balance });
         }
 
-        public void Debit(decimal amount)
+        public void Debit(DateTime _date, decimal _amount)
         {
-            if (balance < amount)
+            if (balance < _amount)
                 throw new Exception("Insufficient Funds.");
-            balance -= amount;   
+            balance -= _amount;   
+            statement.Add(new Transaction() { date = _date, credit = 0, debit = _amount, balance = balance });
+
+        }
+
+        public void PrintStatement()
+        {
+            PrintHeader();
+
+            foreach(Transaction transaction in statement)
+            {
+                Console.WriteLine("{0,-12} || {1,10} || {2,-10} || {3,10}",
+                                  transaction.date.ToShortDateString(),
+                                  transaction.credit,
+                                  transaction.debit,
+                                  transaction.balance
+                                 );
+            }
+        }
+
+
+        private void PrintHeader()
+        {
+            Console.WriteLine("{0,-12} || {1,10} || {2,-10} || {3,10}", "Date", "Credit", "Debit", "Balance");
+
+        }
+
+
+        public void main(){
+            Credit(DateTime.Now, 2015);
+            Debit(DateTime.Today, 1000);
+            Console.WriteLine("hello world");
+            PrintStatement();
+
         }
     }
 }
