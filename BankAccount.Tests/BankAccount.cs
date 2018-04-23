1d193088d1ccb48f9c4f95f3205106a169377e57
durranee <durranee@hotmail.com>
2018-04-23 19:55:45 +0100

    tests fixed and now user can debit the account

--- BankAccount.Tests/BankAccount.cs
+++ BankAccount.Tests/BankAccount.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;  
+
 namespace BankAccount.Tests
 {
-    public class BankAccount
-    {
-        public BankAccount()
+    [TestClass]
+    public class BankAccountTest
+    {   
+        [TestMethod]
+        public void AddGivenAmountToBalance()
         {
+            BankAccount account = new BankAccount();
+            account.Credit(1000);
+            Assert.AreEqual(1000, account.GetBalance());
+        }
+
+        [TestMethod]
+        public void TakeAwayGivenAmountFromBalance()
+        {
+            BankAccount account = new BankAccount();
+            account.Credit(1000);
+            account.Debit(200);
+            Assert.AreEqual(800, account.GetBalance());
         }
     }
 }
--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -3,8 +3,26 @@ namespace BankAccount
 {
     public class BankAccount
     {
+        decimal balance;
+
         public BankAccount()
         {
+            balance = 0;
+        }
+
+        public decimal GetBalance(){
+            return balance;
+        }
+
+        public void Credit(decimal amount)
+        {
+            balance += amount;    
+        }
+
+        public void Debit(decimal amount)
+        {
+            balance -= amount;
+            
         }
     }
 }
